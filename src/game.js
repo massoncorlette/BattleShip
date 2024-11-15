9e99a78678a3de3007ae1df690d3ee224a6c972d
massoncorlette <massoncorlette07@gmail.com>
2024-11-15 04:25:43 -0500

    refactor: gameObjects adjusted to store coordinates as a adjacency list for possible AI logic later on, placing and storing all ships for Player is fully functional

--- src/game.js
+++ src/game.js
@@ -20,13 +20,17 @@ export async function startGame() {
 }
 
 export function placeAllShips(playersBoard) {
+  
+  function getRandomDirection() {
+    return Math.random() < 0.5 ? "X" : "Y";
+  }
 
     playersBoard.ships.forEach((ship) => {
-      shipPlacements(playersBoard,ship.shipType,ship.length,"X")
+      let randomDirection = getRandomDirection()
+      shipPlacements(playersBoard,ship.shipType,ship.length,randomDirection)
     })
     console.log(playersBoard.placedShips);
 }
-
 
 export function shipPlacements(playersBoard,shipType,length,direction) {
 
@@ -43,10 +47,9 @@ export function shipPlacements(playersBoard,shipType,length,direction) {
 
   console.log(playersBoard);
 
-  while (checkShipPlacement(playersBoard.placedShips,length,direction,generatedCoordinates) === false) {
+  while (checkShipPlacement(playersBoard.listOfShipsCoordinates,length,direction,generatedCoordinates) === false) {
     generatedCoordinates = generateCoordinates();
   }
-
   // the while statement returning final returned valid coordinates
   playersBoard.placeShip(shipType,length,direction,generatedCoordinates);
 
@@ -54,27 +57,26 @@ export function shipPlacements(playersBoard,shipType,length,direction) {
   console.log(playersBoard.placedShips);
 
   return true;
-
   // return commodore.playersBoard.placedShips;
 }
 
-export function checkShipPlacement(playersShips,length,direction,coordinates) {
+// check for already placed Ships
+export function checkShipPlacement(allShipCoordinates,length,direction,coordinates) {
 
     let testedCoordinates = [];
     testedCoordinates.push(coordinates[0]);
     testedCoordinates.push(coordinates[1]);
 
-    // check for already placed Ships
     if (direction == "X") {
       for (let i=0;i<length;i++) {
-        if (checkForPlacedShips(playersShips,testedCoordinates) == true) {
+        if (checkForPlacedShips(allShipCoordinates,testedCoordinates) == true) {
           return false;
         }
         testedCoordinates[0] = testedCoordinates[0] + 1;
       }
     } else if (direction == "Y") {
       for (let i=0;i<length;i++) {
-        if (checkForPlacedShips(playersShips,testedCoordinates) == true) {
+        if (checkForPlacedShips(allShipCoordinates,testedCoordinates) == true) {
           return false;
         }
         testedCoordinates[1] = testedCoordinates[1] + 1;
@@ -87,17 +89,17 @@ export function checkShipPlacement(playersShips,length,direction,coordinates) {
     return true;
   }
 
-
 export function checkForPlacedShips(playersPlacedShips, coordinates) {
     for (let i = 0; i < playersPlacedShips.length; i++) {
-      if (playersPlacedShips[i].coordinates[0] === coordinates[0] && playersPlacedShips[i].coordinates[1] === coordinates[1]) {
+      for (let j = 0; j < playersPlacedShips[i].length; j++) {
+        if (playersPlacedShips[i][j][0] === coordinates[0] && playersPlacedShips[i][j][1] === coordinates[1]) {
           return true;  
+        }
       }
     }
   return false;  
 }
 
-//Pure Function decoupled away from shipPlacements()
 export function checkForOutOfBounds(coordinates,length,direction) {
 
     if (direction === "X") {
--- src/gameObjects.js
+++ src/gameObjects.js
@@ -3,6 +3,7 @@
 
 const Ship = function(shipType,length,direction,coordinates) {
   const hitDetection = 0;
+  const allCoordinates = []; // once ship is placed valid, it's coordinates here
 
   function hit(damage) {
     if (damage) {
@@ -19,25 +20,43 @@ const Ship = function(shipType,length,direction,coordinates) {
     return false;
   }
 
+  function getAllShipCoordinates() {
+    if (direction == "X"){
+      for (let i=0;i<length;i++) {
+        allCoordinates.push([coordinates[0] + i,coordinates[1]])
+      }
+    } else if (direction == "Y"){
+        for (let i=0;i<length;i++) {
+          allCoordinates.push([coordinates[0],coordinates[1] + i])
+        }
+    }
+    console.log(allCoordinates);
+    return allCoordinates;
+  };
   return {
     hit:hit,
     isSunk:isSunk,
+    getAllShipCoordinates:getAllShipCoordinates,
     shipType:shipType,
     length:length,
     direction:direction,
     coordinates:coordinates,
+    allCoordinates,
   }
 }
 
 
 export const Gameboard = function(computer) {
 
-  const board = []; // [x][y] //keep track of missed attacks, storing attacks here
+  const board = []; // [x][y] //keep track of missed attacks, storing attacks here?
 
   const ships = [];
-  ships.push(new Ship("Carrier", 5), new Ship("Battleship", 4), new Ship("Cruiser", 3), new Ship("Submarine", 3));
+  ships.push(new Ship("Carrier", 5), new Ship("Battleship", 4), new Ship("Cruiser", 3), new Ship("Submarine", 3), new Ship("Destroyer", 2));
 
-  const placedShips = [];   // adjacency list of placed ships
+  const placedShips = [];   
+
+  const listOfShipsCoordinates = []; //adjacency list of coordinates for all placed ships
+
 
   function placeShip(shipType,length,direction,coordinates) {
 
@@ -46,6 +65,10 @@ export const Gameboard = function(computer) {
     }
 
     const newShip = Ship(shipType,length,direction,coordinates);
+
+    const allShipCoordinates = newShip.getAllShipCoordinates(); // returning and initiating function
+    listOfShipsCoordinates.push(allShipCoordinates);
+
     placedShips.push(newShip);
   }
 
@@ -62,6 +85,7 @@ export const Gameboard = function(computer) {
     receiveAttack:receiveAttack,
     board:board,
     placedShips:placedShips,
+    listOfShipsCoordinates:listOfShipsCoordinates,
   }
 }
 
