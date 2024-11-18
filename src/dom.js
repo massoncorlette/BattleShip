a33ae9f0ba9fd7cfd915f65476648b2d90a2d978
Masson Corlette <massoncorlette07@gmail.com>
2024-11-18 13:26:37 -0500

    feat: making grid - adding string ID's to each cell for coordinates, convert string arrays in another function.

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,8 @@
 // render objects in DOM here
 
+import { startGame } from "./game";
+import { Player } from "./gameObjects";
+
 
 
 export function setupDOM() {
@@ -11,9 +14,33 @@ export function setupDOM() {
 
   function loadGame() {
     const main = document.getElementById('main');
+    const board = document.createElement('div');
+    board.id = 'gameBoard';
 
     main.innerHTML = '';
+    main.appendChild(board);
+    loadBoard();
 
   }
+
+  function loadBoard() {
+
+    let board = document.querySelector('#gameBoard');
+
+    for (let i=0;i<10;i++) {
+      const row = document.createElement('div');
+      row.classList.add('gridRows');
+      board.appendChild(row);
+  
+      for (let j=0;j<10;j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('gridCells');
+        cell.id = [i + 1,j + 1];
+        row.appendChild(cell);
+      } 
+      board.appendChild(row);
+    }
+  }
+
 
   
--- src/game.js
+++ src/game.js
@@ -7,11 +7,11 @@ let commodoreTwo = null; // two being human or computer
 
 
 export function initializeApp() {
-    console.log('App initialized with core logic.');
-    // render the page with a DOM function here
-    // Any additional logic initialization here
-    startGame();
-  }
+  console.log('App initialized with core logic.');
+  // render the page with a DOM function here
+  // Any additional logic initialization here
+ 
+}
 
 export async function startGame() {
   commodoreOne = new Player();
@@ -25,10 +25,10 @@ export function placeAllShips(playersBoard) {
     return Math.random() < 0.5 ? "X" : "Y";
   }
 
-    playersBoard.ships.forEach((ship) => {
-      let randomDirection = getRandomDirection()
-      shipPlacements(playersBoard,ship.shipType,ship.length,randomDirection)
-    })
+  playersBoard.ships.forEach((ship) => {
+    let randomDirection = getRandomDirection()
+    shipPlacements(playersBoard,ship.shipType,ship.length,randomDirection)
+  })
 }
 
 export function shipPlacements(playersBoard,shipType,length,direction) {
@@ -60,53 +60,53 @@ export function shipPlacements(playersBoard,shipType,length,direction) {
 // check for already placed Ships
 export function checkShipPlacement(allShipCoordinates,length,direction,coordinates) {
 
-    let testedCoordinates = [];
-    testedCoordinates.push(coordinates[0]);
-    testedCoordinates.push(coordinates[1]);
+  let testedCoordinates = [];
+  testedCoordinates.push(coordinates[0]);
+  testedCoordinates.push(coordinates[1]);
 
-    if (direction == "X") {
-      for (let i=0;i<length;i++) {
-        if (checkForPlacedShips(allShipCoordinates,testedCoordinates) == true) {
-          return false;
-        }
-        testedCoordinates[0] = testedCoordinates[0] + 1;
+  if (direction == "X") {
+    for (let i=0;i<length;i++) {
+      if (checkForPlacedShips(allShipCoordinates,testedCoordinates) == true) {
+        return false;
       }
-    } else if (direction == "Y") {
-      for (let i=0;i<length;i++) {
-        if (checkForPlacedShips(allShipCoordinates,testedCoordinates) == true) {
-          return false;
-        }
-        testedCoordinates[1] = testedCoordinates[1] + 1;
+      testedCoordinates[0] = testedCoordinates[0] + 1;
+    }
+  } else if (direction == "Y") {
+    for (let i=0;i<length;i++) {
+      if (checkForPlacedShips(allShipCoordinates,testedCoordinates) == true) {
+        return false;
       }
+      testedCoordinates[1] = testedCoordinates[1] + 1;
     }
-    // check for ship staying in bounds
-    if (checkForOutOfBounds(coordinates,length,direction) == false) {
-      return false;
-    }
-    return true;
   }
+  // check for ship staying in bounds
+  if (checkForOutOfBounds(coordinates,length,direction) == false) {
+    return false;
+  }
+  return true;
+}
 
 export function checkForPlacedShips(playersPlacedShips, coordinates) {
-    for (let i = 0; i < playersPlacedShips.length; i++) {
-      for (let j = 0; j < playersPlacedShips[i].length; j++) {
-        if (playersPlacedShips[i][j][0] === coordinates[0] && playersPlacedShips[i][j][1] === coordinates[1]) {
-          return true;  
-        }
+  for (let i = 0; i < playersPlacedShips.length; i++) {
+    for (let j = 0; j < playersPlacedShips[i].length; j++) {
+      if (playersPlacedShips[i][j][0] === coordinates[0] && playersPlacedShips[i][j][1] === coordinates[1]) {
+        return true;  
       }
     }
+  }
   return false;  
 }
 
 export function checkForOutOfBounds(coordinates,length,direction) {
 
-    if (direction === "X") {
-      if ((coordinates[0] + length) > 11) {
-        return false;
-      }
-    } else if (direction === "Y") {
-      if ((coordinates[1] + length) > 11) {
-        return false;
-      }
+  if (direction === "X") {
+    if ((coordinates[0] + length) > 11) {
+      return false;
     }
-    return true;
+  } else if (direction === "Y") {
+    if ((coordinates[1] + length) > 11) {
+      return false;
+    }
+  }
+  return true;
 }
