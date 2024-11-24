704721b2cb982a0548e6a7103b832e2d83590268
massoncorlette <massoncorlette07@gmail.com>
2024-11-24 00:37:31 -0500

    feat: setting up start screen, and 1p and 2p modes

--- src/dom.js
+++ src/dom.js
@@ -5,42 +5,80 @@ import { Player } from "./gameObjects";
 
 
 
-export function setupDOM() {
+export function loadScreen() {
   
-    const startBtn = document.getElementById('startBtn');
+  const startDiv = document.getElementById('startBtnDiv');
+  const startBtn = document.getElementById('startBtn');
 
-    startBtn.addEventListener('click', loadGame)
+  startBtn.addEventListener('mouseenter', () => {
+    startBtn.innerHTML = '&#9654 BOOT';
+  })
+
+  startBtn.addEventListener('mouseleave', () => {
+    startBtn.innerHTML = 'BOOT';
+  })
+
+  const onePBtn = document.createElement('button');
+  onePBtn.classList.add('modeBtn');
+  onePBtn.innerHTML = '1P';
+  const twoPBtn = document.createElement('button');
+  twoPBtn.classList.add('modeBtn');
+  twoPBtn.innerHTML = '2P';
+
+
+  startBtn.addEventListener('click', () => {
+    startDiv.removeChild(startBtn);
+    startDiv.id ='startBtnDivOpt';
+    startDiv.appendChild(onePBtn);
+    startDiv.appendChild(twoPBtn);
+  })
+
+  onePBtn.addEventListener('mouseenter', () => {
+    onePBtn.innerHTML = '&#9654 1P';
+  })
+
+  onePBtn.addEventListener('mouseleave', () => {
+    onePBtn.innerHTML = '1P';
+  })
+
+  twoPBtn.addEventListener('mouseenter', () => {
+    twoPBtn.innerHTML = '&#9654 2P';
+  })
+
+  twoPBtn.addEventListener('mouseleave', () => {
+    twoPBtn.innerHTML = '2P';
+  })
+}
+
+function loadGame() {
+  const main = document.getElementById('main');
+  const board = document.createElement('div');
+  board.id = 'gameBoard';
+
+  main.innerHTML = '';
+  main.appendChild(board);
+  loadBoard();
+  getCoordinates();
+}
+
+function loadBoard() {
+
+  let board = document.querySelector('#gameBoard');
+
+  for (let i=0;i<10;i++) {
+    const row = document.createElement('div');
+    row.classList.add('gridRows');
+    board.appendChild(row);
+
+    for (let j=0;j<10;j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('gridCells');
+      cell.id = [j + 1,10 - i];
+      row.appendChild(cell);
+    } 
+    board.appendChild(row);
   }
-
-  function loadGame() {
-    const main = document.getElementById('main');
-    const board = document.createElement('div');
-    board.id = 'gameBoard';
-
-    main.innerHTML = '';
-    main.appendChild(board);
-    loadBoard();
-    getCoordinates();
-  }
-
-  function loadBoard() {
-
-    let board = document.querySelector('#gameBoard');
-
-    for (let i=0;i<10;i++) {
-      const row = document.createElement('div');
-      row.classList.add('gridRows');
-      board.appendChild(row);
-  
-      for (let j=0;j<10;j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('gridCells');
-        cell.id = [j + 1,10 - i];
-        row.appendChild(cell);
-      } 
-      board.appendChild(row);
-    }
-  }
+}
 
   function getCoordinates() {
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { initializeApp } from './game.js';
-import { setupDOM } from './dom.js';
+import { loadScreen as loadScreen } from './dom.js';
 import './style.css';
 
 console.log('test');
 
 initializeApp();
-setupDOM();
+loadScreen();
