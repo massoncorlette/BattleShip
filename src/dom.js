33d71d234286bdbce7dfd017c37ac3e64d2b8dc9
massoncorlette <massoncorlette07@gmail.com>
2024-12-01 01:02:30 -0500

    refactor: attempting to use interactjs lib for drag and drop instead of html5s

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,7 @@
 // render objects in DOM here
 
+import interact from 'interactjs'
+
 import { startGame,commodoreOne,commodoreTwo } from "./game";
 import { Player } from "./gameObjects";
 
@@ -119,82 +121,63 @@ function loadShips(currentPlayer) {
     shipElement.appendChild(img);
 
     shipContainer.appendChild(shipElement);
-    draggableImages().setDraggable(shipElement);
+    setDragDrop().setDraggable(shipElement);
   }
 
-  function draggableImages() {
-    function setDraggable(ship) {
-      ship.draggable = 'true';
-      ship.addEventListener('dragstart', function(event) {
-        console.log(event);
-        setDrop(ship);
-      })
+  function setDragDrop() {
+  // enable draggables to be dropped into this
+  interact('.dropzone').dropzone({
+    // only accept elements matching this CSS selector
+    accept: '#yes-drop',
+    // Require a 75% element overlap for a drop to be possible
+    overlap: 0.75,
+
+    // listen for drop related events:
+
+    ondropactivate: function (event) {
+      // add active dropzone feedback
+      event.target.classList.add('drop-active')
+    },
+    ondragenter: function (event) {
+      var draggableElement = event.relatedTarget
+      var dropzoneElement = event.target
+
+      // feedback the possibility of a drop
+      dropzoneElement.classList.add('drop-target')
+      draggableElement.classList.add('can-drop')
+      draggableElement.textContent = 'Dragged in'
+    },
+    ondragleave: function (event) {
+      // remove the drop feedback style
+      event.target.classList.remove('drop-target')
+      event.relatedTarget.classList.remove('can-drop')
+      event.relatedTarget.textContent = 'Dragged out'
+    },
+    ondrop: function (event) {
+      event.relatedTarget.textContent = 'Dropped'
+    },
+    ondropdeactivate: function (event) {
+      // remove active dropzone feedback
+      event.target.classList.remove('drop-active')
+      event.target.classList.remove('drop-target')
     }
+  })
 
-    function setDrop(ship) {
-      const boardOneCells = document.querySelectorAll('#gameBoardOne .gridCells');
-      const boardTwoCells = document.querySelectorAll('#gameBoardTwo .gridCells');
-
-      const boardOneRows = document.querySelectorAll('#gameBoardOne .gridRows');
-      const boardTwoRows = document.querySelectorAll('#gameBoardOne .gridRows');
-
-      if (currentPlayer === 'playerOne') {
-        const selectAllCells = boardOneCells;
-        const selectAllRows = boardOneRows;
-        dropListenerEvents(selectAllCells,selectAllRows);
-      } else if (currentPlayer === 'playerTwo') {
-        const selectAllCells = boardTwoCells;
-        const selectAllRows = boardTwoRows;
-        dropListenerEvents(selectAllCells,selectAllRows);
-      }
-
-      function dropListenerEvents(cells,rows) {
-
-        cells.forEach(cell => {
-          cell.addEventListener('dragover', function(event) {
-            event.preventDefault();
-            cell.classList.add('gridCellsDragOver');
-          })
-  
-          cell.addEventListener('dragleave', function(event) {
-            event.preventDefault();
-            cell.classList.remove('gridCellsDragOver');
-          })
+  interact('.drag-drop')
+    .draggable({
+      inertia: true,
+      modifiers: [
+        interact.modifiers.restrictRect({
+          restriction: 'parent',
+          endOnly: true
         })
-  
-        rows.forEach(row => {
-          row.addEventListener('dragover', function(event) {
-            event.preventDefault();
-          })
-        })
-  
-        rows.forEach(row => {
-          row.addEventListener('drop', function(event) {
-            ship.style.position = 'absolute'; 
-            row.append(ship);
-
-            ship.draggable = false;
-
-            ship.removeEventListener('dragstart', handleDragStart);
-          })
-        })
-        function handleDragStart(event) {
-          console.log(event);
-          setDrop(ship);
-        }
-      }
-
-      
-      function highlightCells(cellID, shipLength) {
-
-      }
-
-    }
-
-    return {
-      setDraggable:setDraggable,
-    }
+      ],
+      autoScroll: true,
+      // dragMoveListener from the dragging demo above
+      listeners: { move: dragMoveListener }
+    })
   }
+
 
   divImages(Carrier,'carrier');
   divImages(BattleShip,'battleship');
